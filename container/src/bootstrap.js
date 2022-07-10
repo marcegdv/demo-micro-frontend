ae2cb7a506fc53c9a19a2fd15c7650a92b42754e
marcegdv <mgdv79@gmail.com>
2022-07-10 01:04:28 -0300

    .

--- container/src/bootstrap.js
+++ container/src/bootstrap.js
@@ -1,7 +1,7 @@
 import('microFrontEnd4/MicroFrontEnd4Index')
     .then(module => { console.log('mfe4 loaded!'); return module; })
     .catch(() => console.log('Error: mfe4 not loaded'));
-    
+
 import('microFrontEnd1/MicroFrontEnd1Index')
     .then(module => { console.log('mfe1 loaded!'); return module; })
     .catch(() => console.log('Error: mfe1'));
@@ -13,7 +13,5 @@ import('microFrontEnd2/MicroFrontEnd2Index')
 import('microFrontEnd3/MicroFrontEnd3Index')
     .then(module => { console.log('mfe3 loaded!'); return module; })
     .catch(() => console.log('Error: mfe3'));
-
-
 
 console.log('Container!');
