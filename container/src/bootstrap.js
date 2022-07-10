1b020b54133d22040df7ba99fea62a6b1ba550bf
marcegdv <mgdv79@gmail.com>
2022-07-10 01:03:50 -0300

    fix error in loading micro-frontend

--- container/src/bootstrap.js
+++ container/src/bootstrap.js
@@ -1,6 +1,19 @@
-import 'microFrontEnd1/MicroFrontEnd1Index';
-import 'microFrontEnd2/MicroFrontEnd2Index';
-import 'microFrontEnd3/MicroFrontEnd3Index';
+import('microFrontEnd4/MicroFrontEnd4Index')
+    .then(module => { console.log('mfe4 loaded!'); return module; })
+    .catch(() => console.log('Error: mfe4 not loaded'));
+    
+import('microFrontEnd1/MicroFrontEnd1Index')
+    .then(module => { console.log('mfe1 loaded!'); return module; })
+    .catch(() => console.log('Error: mfe1'));
+
+import('microFrontEnd2/MicroFrontEnd2Index')
+    .then(module => { console.log('mfe2 loaded!'); return module; })
+    .catch(() => console.log('Error: mfe2'));
+
+import('microFrontEnd3/MicroFrontEnd3Index')
+    .then(module => { console.log('mfe3 loaded!'); return module; })
+    .catch(() => console.log('Error: mfe3'));
+
 
 
 console.log('Container!');
